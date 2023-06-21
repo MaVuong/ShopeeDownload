77aed847007088554d920bf050f071c37a7817f2
MaVuong <dethichthien@me.com>
2023-06-21 14:13:42 +0700

    Update taongaythang.js
    vcvv

--- JS/taongaythang.js
+++ JS/taongaythang.js
@@ -2,7 +2,27 @@ var DATETIME_HEADER1 = 'ngày giao';
 var DATETIME_HEADER2 = 'ngay giao';
 var EDIT_TRIGGER1 = 'mã đơn hàng';
 var EDIT_TRIGGER2 = 'ma don hang';
+function formatDate() {
+    
+    var d = new Date();
+    var month = '' + (d.getMonth() + 1);
+    var day = '' + d.getDate();
+    var year = d.getFullYear();
 
+    var gio=d.getHours();
+    var phut=d.getMinutes();
+    var giay=d.getSeconds();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    var str1= [day, month, year].join('/');
+    var str2= [gio, phut, giay].join(':');
+    return str1+" "+str2;
+}
+ 
 function onEdit(e) {  
  
   const row = e.range.getRow();
@@ -13,7 +33,7 @@ function onEdit(e) {
   if(row<2){
     return;
   }
-  // as.getRange(row, 11).setValue(JSON.stringify(arrFirstRow));  
+
   var hieu_col=col-1;
   if(hieu_col>0){
     if(arrFirstRow.length>hieu_col){
@@ -38,7 +58,9 @@ function onEdit(e) {
           }
 
           if(idTim<20){
-            as.getRange(row, idTim).setValue(new Date()).setNumberFormat("dd/mm/yyyy hh:mm:ss");
+            var date_time=formatDate();
+            as.getRange(row, idTim).setValue(date_time);//.setNumberFormat("dd/mm/yyyy hh:mm:ss");
+            AutoFillAll(as,hieu_col,idTim-1,date_time,row-1);
             //as.getRange(row, 10).setValue(e.getValues()+"vldc");
           }else{
             //as.getRange(row, 10).setValue("Loi roi ko thim thay header:"+allLine);
@@ -58,3 +80,56 @@ function onEdit(e) {
   
   
 }
+
+function AutoFillAll(as,id_mdh,id_thoigian,strlastDate,bat_dau) {
+    var arrAll=as.getDataRange().getValues();
+    if(arrAll.length<bat_dau){
+      return;
+    }
+    if(bat_dau<1){
+      bat_dau=1;
+    }
+    var MM=id_thoigian>id_mdh?id_thoigian:id_mdh;
+    MM=MM-1;
+    var last_time="";
+    for(var tk=bat_dau;tk<arrAll.length;tk++){
+       var arrDongT=arrAll[tk];
+
+       var strMDH=arrDongT[id_mdh]+"";
+       if(strMDH=="undefined"){
+        continue;
+       }
+       if(strMDH.length<2){
+        continue;
+       }
+       var strTG=arrDongT[id_thoigian]+"";
+       var allowFill=false;
+       if(strTG=="undefined"){
+          allowFill=true;
+       }
+      if(strTG.length<2){
+          allowFill=true;
+        }
+        if(allowFill){
+          as.getRange((tk+1), (id_thoigian+1)).setValue(strlastDate);
+        }
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
